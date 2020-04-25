257864ee23dbb0d0529363ad735a580118862cb5
ayush577 <agandhi62@gmail.com>
2020-04-25 22:17:30 +0530

    Job-listing Development complete. Need to improve Design responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FileringService } from "./filering.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [FileringService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
